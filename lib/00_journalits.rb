a9544d8d146537b972ef6e2952b5a829b1722c67
Redfact <tantelyrandria@ymail.com>
2020-04-09 02:45:35 +0200

    Exo journalist

---
+++ lib/00_journalits.rb
@@ -0,0 +1,126 @@
+def nombre_valeurs (array)
+	puts "Il y a #{array.length} journalistes dans la table"
+	return array.length
+end
+
+def Nb_Number_Array (array)
+	res = 0
+	array.length.times do |i|
+#  "str =~ /\d/" fonction regex , renvoy vrai si str contient un entier
+		if (array[i] =~ /\d/) then
+			res +=1 
+		end
+	end
+	puts "Il y a #{res} handles avec chiffres"
+	return res 	
+end
+
+def Nb_Mot_Array (array, mot)
+	res = 0
+	array.length.times do |i|
+		if (Contain_Chars_succ(array[i],mot)) then
+			#puts array[i].downcase
+			res +=1 
+		end
+	end
+	puts "Il y a #{res} handles contenant #{mot}"
+	return res 	
+end
+
+#downcase met le text à la minuscule
+def  Contain_Chars_succ(str,mot)
+	index_mot =0
+	str.length.times do |i|
+		if( str[i].downcase.eql?(mot[index_mot].downcase) )
+			index_mot+=1
+		end
+		if( index_mot > mot.length-1)
+			return true 
+		end
+	end
+	return false
+end
+
+#Verifie si Str contient une lettre majuscule
+def Contain_Uppercase_Char str
+	if /[[:upper:]]/.match(str) then return true else return false end 
+end
+
+def Nb_Str_contain_upper(array)
+	res = 0
+	array.length.times do |i|
+		if (Contain_Uppercase_Char array[i]) then
+			res +=1 
+		end
+	end
+	puts "Il y a #{res} handles contenant au moin une lette en Majuscule"
+	return res 
+end
+
+def Nb_Str_Begin_upper(array)
+	res = 0
+	array.length.times do |i|
+		if ( Contain_Uppercase_Char array[i][1]) then
+			res +=1 
+		end
+	end
+	puts "Il y a #{res} handles commencant par une Majuscule"
+	return res 
+end
+
+def Nb_underscore str
+	res = 0
+	str.length.times do |i|
+		if ( str[i] == '_') then
+			res +=1 
+		end
+	end
+	return res 
+end
+
+def Nb_underscore_array array
+	res = 0
+	array.length.times do |i|
+		res+= Nb_underscore array[i]
+	end
+	puts "Il y a #{res} underscore au total"
+	return res 
+end
+
+def Pos_Handle(array,handle)
+	array.length.times do |i|
+		if( array[i].eql?(handle)) then
+			puts "Le hande : #{handle} se trouve à la position #{i} du tableau"
+			return i
+		end
+	end
+	return -1
+end
+
+def trie_alphabetic array
+	print array.sort
+end
+
+def shortest_handles (array , nb)
+	tmp = array.sort_by(&:length)
+	puts "Les #{nb} handles les plus courts :"
+	for i in 0..nb-1
+		puts"#{tmp[i]} "
+	end
+	puts ""
+end
+
+def perform
+	journalistes = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+	nombre_valeurs journalistes
+	Nb_Number_Array journalistes
+	Nb_Mot_Array(journalistes,"Aude")
+	Nb_Str_contain_upper journalistes
+	Nb_Str_Begin_upper journalistes
+	Nb_underscore_array journalistes
+	shortest_handles(journalistes,10)
+	Pos_Handle(journalistes,"@epenser")
+	#trie_alphabetic journalistes
+
+end
+perform
